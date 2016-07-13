44881ddeb5f8021b31f2ac439fe1e8c2955c83ad
Maisur <muhammad.rahman2@gmail.com>
2016-07-13 11:03:37 +0700

    Add Laravel File

---
+++ resources/views/foot.blade.php
@@ -0,0 +1,46 @@
+<!-- Le javascript
+================================================== -->
+
+<!-- Placed at the end of the document so the pages load faster -->
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
+<script src="{{URL::asset('assets/bootstrap/js/bootstrap.min.js')}}"></script>
+
+<!-- include carousel slider plugin  -->
+<script src="{{URL::asset('assets/js/owl.carousel.min.js')}}"></script>
+
+<!-- include equal height plugin  -->
+<script src="{{URL::asset('assets/js/jquery.matchHeight-min.js')}}"></script>
+
+<!-- include jquery list shorting plugin plugin  -->
+<script src="{{URL::asset('assets/js/hideMaxListItem.js')}}"></script>
+
+<!-- include jquery.fs plugin for custom scroller and selecter  -->
+<script src="{{URL::asset('assets/plugins/jquery.fs.scroller/jquery.fs.scroller.js')}}"></script>
+<script src="{{URL::asset('assets/plugins/jquery.fs.selecter/jquery.fs.selecter.js')}}"></script>
+
+
+<!-- include custom script for site  -->
+<script src="{{URL::asset('assets/js/script.js')}}"></script>
+
+<script>
+
+
+</script>
+
+
+<!-- include jquery autocomplete plugin  -->
+
+<script type="text/javascript" src="{{URL::asset('assets/plugins/autocomplete/jquery.mockjax.js')}}"></script>
+<script type="text/javascript" src="{{URL::asset('assets/plugins/autocomplete/jquery.autocomplete.js')}}"></script>
+<script type="text/javascript" src="{{URL::asset('assets/plugins/autocomplete/usastates.js')}}"></script>
+
+<script type="text/javascript" src="{{URL::asset('assets/plugins/autocomplete/autocomplete-demo.js')}}"></script>
+<!--Start of Zopim Live Chat Script-->
+<script type="text/javascript">
+window.$zopim||(function(d,s){var z=$zopim=function(c){z._.push(c)},$=z.s=
+d.createElement(s),e=d.getElementsByTagName(s)[0];z.set=function(o){z.set.
+_.push(o)};z._=[];z.set._=[];$.async=!0;$.setAttribute("charset","utf-8");
+$.src="//v2.zopim.com/?3uz10272DTasXZZvzrmoW51U1rV0qoBS";z.t=+new Date;$.
+type="text/javascript";e.parentNode.insertBefore($,e)})(document,"script");
+</script>
